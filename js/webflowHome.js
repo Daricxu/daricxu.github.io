84830d7d6197dd66e5b66acd3a95c1b216fd19fe
Daric Xu <xugo@mail.uc.edu>
2021-04-14 11:31:44 +0800

    Javascript conflicts with webflow

--- js/webflowHome.js
+++ js/webflowHome.js
@@ -1,3 +1,81 @@
+const texts = document.getElementsByClassName("chaosText smaller");
+const container = document.querySelector(".smallerContainer");
+
+const minor = document.getElementById("minor");
+const major = document.getElementById("major");
+const mega = document.getElementById("mega");
+
+const stretch = document.getElementById("stretch");
+const step = document.getElementById("step");
+const leap = document.getElementById("leap");
+
+const evolution = document.getElementById("evolution");
+const disruption = document.getElementById("disruption");
+const invention = document.getElementById("invention");
+
+// const Ptop = ["10%", "43%", ""];
+const Pleft = ["0%", "12%", "24%", "36%", "48%", "60%", "72%", "84%", "90%"];
+
+let TcurrentItem = 0;
+let LcurrentItem = 0;
+
+
+var id = null;
+
+function chaosFunction() {
+
+    clearInterval(id);
+    id = setInterval(chaos, 1000);
+
+    function chaos() {
+        for (i = 0; i < texts.length; i++) {
+            TcurrentItem = Math.floor(Math.random() * 100);
+            texts[i].style.top = TcurrentItem + "%";
+        }
+        for (i = 0; i < texts.length; i++) {
+
+            LcurrentItem = Math.floor(Math.random() * Pleft.length);
+
+            texts[i].style.left = Pleft[LcurrentItem];
+        }
+    }
+}
+
+
+container.onmouseenter = function() {
+    clearInterval(id);
+    setTimeout(function() {
+        minor.style = "top:12%; left:0%; transition:2s;";
+        major.style = "top:12%; left:47%; transition:2s;";
+        mega.style = "top:12%; right:0%; transition:2s;";
+
+        stretch.style = "top:45%; left:0%; transition:2s;";
+        step.style = "top:45%; left:48%; transition:2s;";
+        leap.style = "top:45%; right:0%; transition:2s;";
+
+        evolution.style = "top: 78%; left:0%; transition:2s;";
+        disruption.style = "top: 78%; left:45%; transition:2s;";
+        invention.style = "top: 78%; right:0%; transition:2s;";
+    }, 1000);
+}
+document.addEventListener('scroll', function() {
+    clearInterval(id);
+    setTimeout(function() {
+        minor.style = "top:12%; left:0%; transition:2s;";
+        major.style = "top:12%; left:47%; transition:2s;";
+        mega.style = "top:12%; right:0%; transition:2s;";
+
+        stretch.style = "top:45%; left:0%; transition:2s;";
+        step.style = "top:45%; left:48%; transition:2s;";
+        leap.style = "top:45%; right:0%; transition:2s;";
+
+        evolution.style = "top: 78%; left:0%; transition:2s;";
+        disruption.style = "top: 78%; left:45%; transition:2s;";
+        invention.style = "top: 78%; right:0%; transition:2s;";
+    }, 300);
+});
+
+
 /*!
  * Webflow: Front-end site library
  * @license MIT
